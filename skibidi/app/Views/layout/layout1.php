7ee4319d9e1799cb8f74fd1a1d77dbbd813af92b
ressi4 <jan.ressler13@seznam.cz>
2024-10-07 11:11:19 +0200

    kijf

---
+++ skibidi/app/Views/layout/layout1.php
@@ -0,0 +1,10 @@
+<html>
+    <head> 
+        <title>Titulek</title>
+    </head> 
+    <body>
+ 
+         <!--Dynamický obsah -->
+         <?= $this->renderSection("content"); ?> 
+    <body>
+</html>
---
+++ skibidi/app/Views/view.php
@@ -0,0 +1,5 @@
+<?=$this->extend("layout/layout1");?>
+
+<?=$this->section("content");?>
+    <h1>Dynamický obsah</h1>
+<?=$this->endSection();?>
